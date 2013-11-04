508d07a1e005467d0d00d8e634637826dd13658e
Pau Fracés <pfraces@gmail.com>
2013-11-05 00:18:54 +0100

    refactored code layout

--- enumer.js
+++ enumer.js
@@ -2,19 +2,25 @@ module.exports = function (obj, callback) {
   var path = [];
 
   var step = function (node) {
+    var index;
+    
+    if (path.length) {
+      index = path[path.length - 1];
+    }
+    
+    var stats = {
+      node: node,
+      path: path,
+      index: index,
+      root: obj
+    };
+    
     var next = function () {
       for (prop in node) {
         path.push(prop);
         step(node[prop]);
         path.pop();
       }
-    };
-
-    var stats = {
-      node: node,
-      path: path,
-      index: path[path.length - 1],
-      root: obj
     };
 
     callback(stats, next);
